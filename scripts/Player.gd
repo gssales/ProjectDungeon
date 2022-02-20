88793aaac9d8e161f80e16220ac1ed26059bddd6
Matheus Godoy <matheusgodoy2000@gmail.com>
2022-02-20 03:50:16 -0300

    prototipo de ui de jogo e de inventário

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -14,40 +14,40 @@ var speed = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Camera.look_at(self.transform.origin, Vector3.UP)
+  $Camera.look_at(self.transform.origin, Vector3.UP)
 
 func _physics_process(delta):
-	var move_vec = Vector3()
-	if Input.is_action_pressed("move_forward"):
-		move_vec += Vector3.FORWARD
-	if Input.is_action_pressed("move_left"):
-		move_vec += Vector3.LEFT
-	if Input.is_action_pressed("move_backward"):
-		move_vec += Vector3.BACK
-	if Input.is_action_pressed("move_right"):
-		move_vec += Vector3.RIGHT
+  var move_vec = Vector3()
+  if Input.is_action_pressed("move_forward"):
+    move_vec += Vector3.FORWARD
+  if Input.is_action_pressed("move_left"):
+    move_vec += Vector3.LEFT
+  if Input.is_action_pressed("move_backward"):
+    move_vec += Vector3.BACK
+  if Input.is_action_pressed("move_right"):
+    move_vec += Vector3.RIGHT
 
-	move_vec = move_vec.normalized()
+  move_vec = move_vec.normalized()
 
-	if move_vec.length() > 0 :
-		var new_yaw = atan2(move_vec.x, move_vec.z) # 0,2*PI
-		var rot = Quat(Vector3(0.0, new_yaw, 0.0))
-		var t = Quat($Model.transform.basis)
-		var interp = t.slerp(rot, 0.2)
-		$Model.transform.basis = Basis(interp)
+  if move_vec.length() > 0 :
+    var new_yaw = atan2(move_vec.x, move_vec.z) # 0,2*PI
+    var rot = Quat(Vector3(0.0, new_yaw, 0.0))
+    var t = Quat($Model.transform.basis)
+    var interp = t.slerp(rot, 0.2)
+    $Model.transform.basis = Basis(interp)
 
-		speed = min(speed + acceleration * delta, max_speed)
-	else:
-		speed = max(speed - acceleration * delta, 0) 
+    speed = min(speed + acceleration * delta, max_speed)
+  else:
+    speed = max(speed - acceleration * delta, 0) 
 
-	move_vec *= speed
+  move_vec *= speed
 
-	if not $DetectGround.is_colliding():
-		move_vec += Vector3.DOWN * GRAVITY * delta
+  if not $DetectGround.is_colliding():
+    move_vec += Vector3.DOWN * GRAVITY * delta
 
-	_velocity = move_vec
+  _velocity = move_vec
 
-	_velocity = move_and_slide_with_snap(_velocity, Vector3.DOWN)
+  _velocity = move_and_slide_with_snap(_velocity, Vector3.DOWN)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
