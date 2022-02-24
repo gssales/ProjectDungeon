3e8db47e876ca7bbe2c2e3832f98a1479c3bf734
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2022-02-23 22:47:40 -0300

    ItemModel interaction

--- item/Item.gd
+++ item/Item.gd
@@ -6,12 +6,14 @@ var _type
 var _name
 var _range
 var damageRange
-
+var params
+  
 func init(params):
   self._type = params._type
   self._name = params._name
   self._range = params._range
   self.damageRange = params.damageRange
+  self.params = params
 
 func damage(multiplier = 1):
   var damage = (randf() * (damageRange[1] - damageRange[0])) + damageRange[0]
