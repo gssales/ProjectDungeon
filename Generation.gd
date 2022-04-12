8a6259792ccd98e58fe0aef841d1e73cb9bb9fbf
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2022-04-12 14:36:48 -0300

    generation

--- Generation.gd
+++ Generation.gd
@@ -10,33 +10,36 @@ var GenericCorridor = preload("res://rooms/GenericCorridor.tscn")
 var Enemy = preload("res://entities/enemy_1/Enemy.tscn")
 var Ally = preload("res://entities/ally_1/Ally.tscn")
 
-var should_spawn_item = true
-
 var matrix_size
-var matrix
 var room_list = []
 
 var initial_position
+
+var room_amount = 0
 
 func _ready():
   randomize()
   matrix_size = max_depth_generation*2
   initial_position = Vector2(matrix_size/2, matrix_size/2)
   
-  generate_matrix()
-  post_generation()
-  var map_node = add_to_map_node()
-  var lights_node = $LightingGenerator.generate_lights(matrix)
+  var m = generate_matrix(matrix_size)
+  m = post_generation(m)
+  
+  var map_node = add_to_map_node(m)
   add_child(map_node)
+  $Player.translate(Vector3(initial_position.x*36, 0 ,initial_position.y*36))
+  return
+  
+  var lights_node = $LightingGenerator.generate_lights(matrix)
   add_child(lights_node)
   
   var item = $ItemSpawner.spawn_random_item(initial_position*36)
   $Items.add_child(item)
   
-  var enemy_inst = Enemy.instance() 
-  $Enemies.add_child(enemy_inst)
-  
-  enemy_inst.translate(Vector3(initial_position.x*36 + 8, 0 ,initial_position.y*36 + 8))
+  for r in room_list:
+    var enemy_inst = Enemy.instance() 
+    $Enemies.add_child(enemy_inst)
+    enemy_inst.translate(Vector3(r.x*36 + 8, 0 ,r.y*36 + 8))
   
   var ally_inst = Ally.instance()
   $Allies.add_child(ally_inst)
@@ -53,162 +56,149 @@ func _ready():
 func _input(event):
   if event.is_action_pressed("take_damage"):
     get_tree().reload_current_scene()
-    
-func add_to_map_node() -> Spatial:
-  var map = Spatial.new()
-  for x in range(matrix_size):
-    for y in range(matrix_size):
-      var r = matrix[x][y]
-      if r != null:
-        map.add_child(matrix[x][y])
-  return map
-    
-func clear_matrix():
-  matrix = []
-  room_list = []
-  matrix.resize(matrix_size)
-  for x in range(matrix_size):
-    matrix[x] = []
-    matrix[x].resize(matrix_size)
-    for y in range(matrix_size):
-      matrix[x][y] = null
-  
-func generate_matrix():
-  clear_matrix()
-  branch(initial_position)
 
-func post_generation():
-  room_list.shuffle()
-  var count_rooms = room_list.size()
-  for r in range(count_rooms/2):
-    var position = room_list[r]
-    var room = matrix[position.x][position.y]
-    
-    var prob = []
-    if position.y-1 >= 0 and not room.portal_up and matrix[position.x][position.y-1] != null:
-      prob.push_back(Vector2.UP)
-    if position.x-1 >= 0 and not room.portal_left and matrix[position.x-1][position.y] != null:
-      prob.push_back(Vector2.LEFT)
-    if position.y+1 < matrix_size and not room.portal_down and matrix[position.x][position.y+1] != null:
-      prob.push_back(Vector2.DOWN)
-    if position.x+1 < matrix_size and not room.portal_right and matrix[position.x+1][position.y] != null:
-      prob.push_back(Vector2.RIGHT)
-      
-    prob.shuffle()
-    var door = prob.pop_back()
-    if door == null:
-      continue
-    
-    var neighbor_position = position + door
-    var neighbor = matrix[neighbor_position.x][neighbor_position.y]
-    
-    if door.is_equal_approx(Vector2.UP):
-      room.portal_up = true
-      neighbor.portal_down = true
-    if door.is_equal_approx(Vector2.LEFT):
-      room.portal_left = true
-      neighbor.portal_right = true
-    if door.is_equal_approx(Vector2.DOWN):
-      room.portal_down = true
-      neighbor.portal_up = true
-    if door.is_equal_approx(Vector2.RIGHT):
-      room.portal_right = true
-      neighbor.portal_left = true
-      
-    matrix[position.x][position.y] = room
-    matrix[neighbor_position.x][neighbor_position.y] = neighbor
-    
+func generate_matrix(matrix_size):
+  var matrix = empty_matrix(matrix_size)
+  var initial_position = Vector2(matrix_size/2, matrix_size/2)
+  room_amount = 0
+  room_list = []
+  branch(matrix, initial_position)
+  return matrix
   
-func map_neighbors(position: Vector2):
-  var x = position.x
-  var z = position.y
-  var neighbors = []
-  if x + 1 < matrix_size and matrix[x + 1][z] == null:
-    neighbors.push_back(Vector2.RIGHT)
-  if x - 1 >= 0 and matrix[x - 1][z] == null:
-    neighbors.push_back(Vector2.LEFT)
-  if z + 1 < matrix_size and matrix[x][z + 1] == null:
-    neighbors.push_back(Vector2.DOWN)
-  if z - 1 >= 0 and matrix[x][z - 1] == null:
-    neighbors.push_back(Vector2.UP)
-  return neighbors
+func empty_matrix(size: int):
+  var m = []
+  m.resize(size)
+  for x in range(size):
+    m[x] = []
+    m[x].resize(size)
+  return m
   
-func branch(position: Vector2, doors = [], corridor_streak = max_corridor_streak, depth = 0):
-  if depth >= max_depth_generation or n_rooms == 0 or matrix[position.x][position.y] != null:
-    return 0
+func branch(matrix, position: Vector2, doors = [], corridor_streak = max_corridor_streak, depth = 0):
+  var neighbors = list_free_neighbors(matrix, position)
+  var count_neighbors = neighbors.size()
   
-  var map_n = map_neighbors(position)
-  map_n.shuffle()
-  
-  var count_n = map_n.size()
-  if count_n == 0:
+  if depth >= max_depth_generation or room_amount == n_rooms or count_neighbors == 0:
     return 0
     
+  neighbors.shuffle()
+  
   var prob_array = []
-  for i in range(1 if count_n == 1 else 4):
-    prob_array.push_back(i < count_n)
+  for i in range(1 if count_neighbors == 1 else 4):
+    prob_array.push_back(i < count_neighbors)
   prob_array.shuffle()
-  print(prob_array)
-  
+
   var generate_corridor = false
   if corridor_streak < max_corridor_streak and randf() < 0.8:
     generate_corridor = true
     corridor_streak += 1
   else:
-    n_rooms -= 1
+    room_amount += 1
+    room_list.push_back(position)
     corridor_streak = 0
     
-  matrix[position.x][position.y] = true
-  var n = 0
-  while n < count_n:
+  matrix[position.x][position.y] = {
+    "type": "corridor" if generate_corridor else "room",
+    "door_up": false,
+    "door_right": false,
+    "door_down": false,
+    "door_left": false
+  }
+
+  for neighbor in neighbors:
     if prob_array.pop_back():
-      var neighbor = map_n.pop_back()
+      if matrix[position.x+neighbor.x][position.y+neighbor.y]:
+        continue
       var next_ = neighbor.rotated(PI)
-      var created = branch(position + neighbor, [next_], corridor_streak, depth + 1)
+      var created = branch(matrix, position + neighbor, [next_], corridor_streak, depth + 1)
       if created:
         doors.push_back(neighbor)
-      else:
-        if depth < max_depth_generation or n_rooms > 0:
-          count_n += 1
-    n += 1
-  
-  if generate_corridor:
-    add_corridor(position, doors)
-  else:
-    add_room(position, doors)
-  return 1
-  
-func add_corridor(position, doors):
-  print("New Corridor ", position, " ", doors)
-  var corridor = GenericCorridor.instance()
-  for d in doors:
-    if d.is_equal_approx(Vector2.UP):
-      corridor.portal_up = true
-    if d.is_equal_approx(Vector2.LEFT):
-      corridor.portal_left = true
-    if d.is_equal_approx(Vector2.DOWN):
-      corridor.portal_down = true
-    if d.is_equal_approx(Vector2.RIGHT):
-      corridor.portal_right = true
-  corridor.translate(Vector3(position.x * room_size.x, 0, position.y * room_size.y))
-#  add_child(corridor)
-  matrix[position.x][position.y] = corridor  
 
-func add_room(position, doors):
-  print("New Room ", position, " ", doors)
-  var room = GenericRoom.instance()
+  for door in doors:
+    set_door(matrix, position, door)
   
-  for d in doors:
-    if d.is_equal_approx(Vector2.UP):
-      room.portal_up = true
-    if d.is_equal_approx(Vector2.LEFT):
-      room.portal_left = true
-    if d.is_equal_approx(Vector2.DOWN):
-      room.portal_down = true
-    if d.is_equal_approx(Vector2.RIGHT):
-      room.portal_right = true
-  room.translate(Vector3(position.x * room_size.x, 0, position.y * room_size.y))
-#  add_child(room)
-  matrix[position.x][position.y] = room
-  room_list.push_back(position)
+  return 1
+
+func list_free_neighbors(matrix, position: Vector2):
+  var x = position.x
+  var z = position.y
+  var size = matrix.size()
+  var neighbors = []
+  if x+1 < size and matrix[x+1][z] == null:
+    neighbors.push_back(Vector2.RIGHT)
+  if x-1 >= 0 and matrix[x-1][z] == null:
+    neighbors.push_back(Vector2.LEFT)
+  if z+1 < size and matrix[x][z+1] == null:
+    neighbors.push_back(Vector2.DOWN)
+  if z-1 >= 0 and matrix[x][z-1] == null:
+    neighbors.push_back(Vector2.UP)
+  return neighbors
   
+func list_full_neighbors(matrix, position: Vector2):
+  var x = position.x
+  var z = position.y
+  var size = matrix.size()
+  var neighbors = []
+  if x+1 < size and matrix[x+1][z] != null and not matrix[x+1][z].door_right:
+    neighbors.push_back(Vector2.RIGHT)
+  if x-1 >= 0 and matrix[x-1][z] != null and not matrix[x-1][z].door_left:
+    neighbors.push_back(Vector2.LEFT)
+  if z+1 < size and matrix[x][z+1] != null and not matrix[x][z+1].door_down:
+    neighbors.push_back(Vector2.DOWN)
+  if z-1 >= 0 and matrix[x][z-1] != null and not matrix[x][z-1].door_up:
+    neighbors.push_back(Vector2.UP)
+  return neighbors
+  
+func set_door(matrix, position: Vector2, door: Vector2):
+  if door.is_equal_approx(Vector2.UP):
+    matrix[position.x][position.y]["door_up"] = true
+  if door.is_equal_approx(Vector2.RIGHT):
+    matrix[position.x][position.y]["door_right"] = true
+  if door.is_equal_approx(Vector2.DOWN):
+    matrix[position.x][position.y]["door_down"] = true
+  if door.is_equal_approx(Vector2.LEFT):
+    matrix[position.x][position.y]["door_left"] = true
+ 
+func post_generation(matrix):
+  room_list.shuffle()
+  for r in range(room_amount/2):
+    var position = room_list[r]
+    
+    var neighbors = list_full_neighbors(matrix, position)
+    neighbors.shuffle()
+    
+    var door = neighbors.pop_back()
+    if door == null:
+      continue
+  
+    var neighbor_position = position + door
+    
+    set_door(matrix, position, door)
+    set_door(matrix, neighbor_position, door.rotated(PI))
+    
+  return matrix
+  
+func add_to_map_node(matrix) -> Spatial:
+  var size = matrix.size()
+  var map = Spatial.new()
+  for x in range(size):
+    for y in range(size):
+      var params = matrix[x][y]
+      if params != null:
+        map.add_child(add_cell(params, Vector2(x, y)))
+  return map
+
+func add_cell(room_params, position):
+  print("New cell ", position, " ", room_params)
+  var cell 
+  if room_params.type == "room":
+    cell = GenericRoom.instance()
+  elif room_params.type == "corridor":
+    cell = GenericCorridor.instance()
+  
+  cell.portal_up = room_params.door_up
+  cell.portal_left = room_params.door_left
+  cell.portal_down = room_params.door_down
+  cell.portal_right = room_params.door_right
+  
+  cell.translate(Vector3(position.x * room_size.x, 0, position.y * room_size.y))
+  return cell
--- rooms/LightingGenerator.gd
+++ rooms/LightingGenerator.gd
@@ -2,6 +2,7 @@ extends Node
 
 var Torch = preload("res://rooms/props/Torch.tscn")
 var rng = RandomNumberGenerator.new()
+var noise = OpenSimplexNoise.new()
 var lights_node
 
 func _ready():
@@ -15,11 +16,12 @@ func generate_lights(matrix):
       var room = matrix[x][y]
       if room == null:
         continue
-      var prob = rng.randfn(0.40, 0.25)
+#      var prob = rng.randfn(0.40, 0.25)
+      var prob = noise.get_noise_2d(x/2, y/2)
       if room.is_in_group("room"):
         light_room(prob, room)
       elif room.is_in_group("corridor"):
-        light_corridor(room)
+        light_corridor(prob, room)
   return lights_node
 
 func light_room(prob, room):
@@ -56,15 +58,29 @@ func light_room(prob, room):
     add_torch_maybe(prob, room.translation + Vector3(16,4.5,7), -PI/2)
 
 func add_torch_maybe(prob, position, rotation = 0):
-  if randf() < prob:
+  prob = noise.get_noise_2d(position.x/4, position.z/4)
+  if 0 < prob:
     lights_node.add_child(new_torch(position, rotation))
-  
-    
+      
 func new_torch(position, rotation = 0):
   var t = Torch.instance()
   t.translate(position)
   t.rotate_y(rotation)
   return t
   
-func light_corridor(corridor):
-  pass
+func light_corridor(prob, corridor):
+  if corridor.portal_up:
+    add_torch_maybe(prob, corridor.translation + Vector3(-3,4.5,-12), PI/2)
+    add_torch_maybe(prob, corridor.translation + Vector3(3,4.5,-8), -PI/2)
+    
+  if corridor.portal_down:
+    add_torch_maybe(prob, corridor.translation + Vector3(-3,4.5,4), PI/2)
+    add_torch_maybe(prob, corridor.translation + Vector3(3,4.5,8), -PI/2)
+    
+  if corridor.portal_left:
+    add_torch_maybe(prob, corridor.translation + Vector3(-12,4.5,-3))
+    add_torch_maybe(prob, corridor.translation + Vector3(-8,4.5,3), PI)
+    
+  if corridor.portal_right:
+    add_torch_maybe(prob, corridor.translation + Vector3(7,4.5,-3))
+    add_torch_maybe(prob, corridor.translation + Vector3(10,4.5,3), PI)
