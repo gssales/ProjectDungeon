9e19c7d24d5d365d41682cedfd808877ed6d5bcd
Matheus Godoy <matheusgodoy2000@gmail.com>
2022-04-21 21:32:05 -0300

    Added main menu + new font in most menus

---
+++ UI/MainMenu/MainMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _ready():
+  $panel/ButtonContainer/New_game.grab_focus()
+
+# Start the game
+func _on_New_game_pressed():
+  get_tree().change_scene("res://Generation.tscn")
+
+# Exit game
+func _on_Quit_pressed():
+  get_tree().quit()
--- UI/PauseMenu/PauseMenu.gd
+++ UI/PauseMenu/PauseMenu.gd
@@ -2,11 +2,13 @@ extends Control
 
 func _ready():
   self.visible = false
+  #$Panel/VBoxContainer/ResumeBTN.grab_focus()
 
 func _input(event):
   if event is InputEventKey:
     if event.is_action_pressed("pause_menu"):
       toggle_pause_menu()
+      $Panel/VBoxContainer/ResumeBTN.grab_focus()
       get_tree().paused = not get_tree().paused
 
 func toggle_pause_menu():
@@ -18,4 +20,10 @@ func _on_ResumeBTN_pressed():
 
 
 func _on_MainMenuBTN_pressed():
+  toggle_pause_menu()
+  get_tree().paused = false
+  get_tree().change_scene("res://UI/MainMenu/MainMenu.tscn")
+
+
+func _on_Quit_pressed():
   get_tree().quit()
