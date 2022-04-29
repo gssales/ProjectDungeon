fdbad518eba06b0955b5d0609431f526fad7de23
Matheus Godoy <matheusgodoy2000@gmail.com>
2022-04-29 00:47:40 -0300

    Added player animations and animation tree

--- player/Player.gd
+++ player/Player.gd
@@ -23,19 +23,26 @@ var weapon_damage = 7
 var weapon_model : Spatial
 var equipped_weapon : Node
 var attack_speed = 0.3
+var has_weapon = false
+
+onready var anim_tree = $Model/lw_polly_char_4_Dir_Mov/AnimationTree
 
 func _ready():
   $Camera.look_at(self.transform.origin, Vector3.UP)
 
-func _input(event):
-  if event is InputEventKey: 
-    if event.is_action_pressed("take_damage"):
-      $Health.take_damage(4)
+#func _input(event):
+#  if event is InputEventKey: 
+#    if event.is_action_pressed("take_damage"):
+#      $Health.take_damage(4)
 
 func _physics_process(delta):
   var move_vec = Vector3()
+  var direction = Vector3()
+  var n_cursor = cursor_pos.normalized()
+  
   if Input.is_action_pressed("move_forward"):
     move_vec += Vector3.FORWARD
+    #irection += Vector3.FORWARD.rotated(Vector3.UP, n_cursor)
   if Input.is_action_pressed("move_left"):
     move_vec += Vector3.LEFT
   if Input.is_action_pressed("move_backward"):
@@ -43,13 +50,19 @@ func _physics_process(delta):
   if Input.is_action_pressed("move_right"):
     move_vec += Vector3.RIGHT
 
+  # direcao deve ser a direcao de movimento, porem em relacao a onde o jogador esta olhando
+  direction = -move_vec.rotated(Vector3.UP, camera_rotation).rotated(Vector3.UP, -$Model.rotation.y)
+  
   move_vec = move_vec.normalized().rotated(Vector3.UP, camera_rotation)
   
-  look_at_cursor()
+  #look_at_cursor() 
+  
+  # Seleciona a animacao baseado no vetor direction
+  anim_tree.set("parameters/idle_walk/blend_position", Vector2(-direction.x, direction.z))
   
   # Combat controls: 
   if Input.is_action_pressed("attack") and attack_cooldown.is_stopped():
-    #print("attack")
+    
     if inventory.get_child_count() != 0:
       equipped_weapon = inventory.get_child(0)
     # If its a sword / melee type weapon -> use hitbox
@@ -77,12 +90,6 @@ func _physics_process(delta):
   
   
   if move_vec.length() > 0 :
-#    var new_yaw = atan2(move_vec.x, move_vec.z) # 0,2*PI
-#    var rot = Quat(Vector3(0.0, new_yaw, 0.0))
-#    var t = Quat($Model.transform.basis)
-#    var interp = t.slerp(rot, 0.2)
-#    $Model.transform.basis = Basis(interp)
-
     speed = min(speed + acceleration * delta, max_speed)
     
     #Se já passou o cooldown e não está correndo -> regen stamina
@@ -91,20 +98,35 @@ func _physics_process(delta):
       #print("Regen stamina")
       
   else:
+    # quando nao esta se movendo olha para a posicao do mouse
+    look_at_cursor()
     speed = max(speed - acceleration * delta, 0)
     #Se já passou o cooldown e não está se movendo -> regen stamina faster
     if $Stamina/StaminaRegenTimer.is_stopped():
       $Stamina.regen_stamina(8*delta)
       #print("Regen stamina")
+    
       
   # Se está se movendo e correndo -> aumentar velocidade e usar stamina
   if move_vec.length() > 0 and Input.is_action_pressed("sprint") and $Stamina.get_stamina() > 0: # temporario, apenas para testar interface
+      # olha na direcao do movimento 
+      #$Model.look_at(global_transform.origin + move_vec, Vector3.UP)
+      var new_yaw = atan2(-move_vec.x, -move_vec.z) # 0,2*PI
+      var rot = Quat(Vector3(0.0, new_yaw, 0.0))
+      var t = Quat($Model.transform.basis)
+      var interp = t.slerp(rot, 0.2)
+      $Model.transform.basis = Basis(interp)
+      
       move_vec *= speed*1.5
       $Stamina.use_stamina(10*delta)
       $Stamina/StaminaRegenTimer.start(1) # cooldown até começar a regenerar (se parar de correr)
       #print("is sprinting")
-  else: #senão velocidade normal
-    move_vec *= speed
+      anim_tree.set("parameters/blend/blend_amount", 1) # esta correndo
+  else:
+    # se nao esta correndo player olha para a posicao do mouse
+    look_at_cursor()
+    anim_tree.set("parameters/blend/blend_amount", 0) # nao esta correndo
+    move_vec *= speed # e velocidade normal
 
   if not $DetectGround.is_colliding():
     move_vec += Vector3.DOWN * GRAVITY * delta
@@ -151,6 +173,7 @@ func _on_Inventory_new_weapon_equipped(new_weapon, new_hitbox):
       # Insert the new weapon model
       $Model/Hand.add_child(weapon_model)
       #attack_hitbox = equipped_weapon.find_node("hitbox_pos").get_child(0) #get the hitbox of the weapon
+      has_weapon = true
       
       var old_hitbox = get_tree().get_nodes_in_group("weapon_hitbox")
       if not old_hitbox.empty():
